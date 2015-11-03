045c2897a1f5bcb1bca989e5d1df7c34821dff2a
Ruslan Kavetsky <rusya.182@gmail.com>
2015-11-03 23:24:41 +0300

    Cat file

--- script.sh
+++ script.sh
@@ -1,14 +1,3 @@
-case $BRANCH in
-	request?* )
-		echo "do not buold request branches"
-		exit 0
-		;;
-esac
-
-
-echo "------------------------------------------"
-git log -1 --pretty=%s
-echo "------------------------------------------"
-git log -3 --pretty=%s
-echo "------------------------------------------"
-git log -3
+echo "---------------------------------"
+cat file.txt
+echo "---------------------------------"
